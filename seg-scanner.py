02a5c077a8a49e0a515ca0735ea36aab460377bd
zabumaphu@mgail.com <zabumaphu@mgail.com>
2024-01-07 12:49:01 -0500

    Python File Made

---
+++ seg-scanner.py
@@ -0,0 +1,13 @@
+"""
+Description: A multi-threaded and async scanner intended to enumerate open ports on a subnet.
+Version: 1.0.0
+Date (Last Updated): January 2024
+Original Author: Aleksa Zatezalo
+"""
+
+##### To Do List#####
+# 1. Make Concurent Port Scanner
+# 2. Make It Asyncio
+# 3. Make It Work For Subnet
+# 4. Make It A Package
+# 5. Test It Thourougly
